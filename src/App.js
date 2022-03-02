d9f4a5e32205d0604fbadf6d7404630760ec95b7
Melissa Zarinnegar <melissahh@live.se>
2022-03-02 18:05:12 +0100

    Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import Footer from './components/Footer';
 
 
 function App() {
   return (
     <div className="App">
-      Webshop in React
+<Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Footer.css'
+import Phone from '../images/mobile-screen.svg'
+import Mail from '../images/envelope.svg'
+import Shoes from '../images/Shoes.png'
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="main-footer">
+      <div className="container">
+        <div className="row">
+             {/* Column2 */}
+             <div className="col"> 
+          <img src={Shoes} alt='' width='150' height='150' />
+        </div>
+          {/* Column1 */}
+          <div className="col">
+            <h4>Meza</h4>
+            <div className="list-styled">
+              <p>
+                Vi har ett maxat utbud med det allra
+                <br />
+                senaste modet från ungefär 350 varumärken
+                <br />
+                som , Nike, Adidas, Jordans, Givency med mera.
+                <br />
+              </p>
+            </div>
+          </div>
+          {/* Column3 */}
+          <div className="col">
+            <h4>Kontakt</h4>
+
+            <p>Gustavslundsvägen 151 D
+                <br />
+                167 51 Bromma</p>
+                <br/>
+            <div className="list-styled">
+              <p><img src={Mail} alt='' width='15' height='15' /> Meza@support.com</p>
+              <p><img src={Phone} alt='' width='15' height='15' /> + 46 702 567 89</p>
+           
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div className="row">
+          <p className="col-sm">
+            &copy;{new Date().getFullYear()} MeZa | All rights reserved |
+            Terms Of Service | Privacy
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
