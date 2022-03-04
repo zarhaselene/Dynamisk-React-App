cc5bc404b77c6c26f561ec6603779357adb06a9d
Zarha Buske <zarha.buske@medieinstitutet.se>
2022-03-04 16:04:05 +0100

    cart error fixed

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,18 +1,19 @@
 import React, { useReducer, useContext, createContext, useEffect } from "react";
+import '../styles/Cart.css'
 
 const CartStateContext = createContext();
 const CartDispatchContext = createContext();
 
 const cartReducer = (state, action) => {
-  switch (action.type) {
+  switch(action.type){
     case "ADD":
-      return [...state, action.item]
+    return [...state, action.item]
     case "REMOVE":
       const newArr = [...state];
       newArr.splice(action.index, 1)
       return newArr;
-    default:
-      throw new Error(`unknown action ${action.type}`)
+    default: 
+    throw new Error(`unknown action ${action.type}`)
   }
 }
 
@@ -25,14 +26,72 @@ export const CartProvider = ({ children }) => {
   useEffect(() => {
     localStorage.setItem('state', JSON.stringify(state))
   }, [state]) 
-  return (
+  return(
     <CartDispatchContext.Provider value={dispatch}>
-      <CartStateContext.Provider value={state}>
-        {children}
-      </CartStateContext.Provider>
+<CartStateContext.Provider value={state}>
+  { children }
+</CartStateContext.Provider>
     </CartDispatchContext.Provider>
   )
 }
 
 export const useCart = () => useContext(CartStateContext);
 export const useDispatch = () => useContext(CartDispatchContext);
+
+const CartItem = ({ product, index, handleRemove, props }) => {
+  return (
+    <>
+      <div className="cart-product">
+      <button className="remove-product" onClick={() => handleRemove(index)}>x</button>
+        <div className="cart-product-detail">
+          <img className="cart-product-image" src={product.url} alt="" />
+          <div className="cart-product-details">
+            <span className="cart-product-title">
+              <b>Product:</b>{product.title}
+            </span>
+            <span className="cart-product-id"><b>ID:</b>{product.id}</span>
+          </div>
+          <div className="cart-product-price">${product.price}</div>
+        </div>  
+      </div>
+      <hr className="cart-hr"/>
+
+    </>
+  );
+};
+
+export default function Store() {
+  const items = useCart();
+  const dispatch = useDispatch();
+  const totalPrice = items.reduce((total, b) => total + b.price, 0);
+
+  const handleRemove = (index) => {
+    dispatch({ type: "REMOVE", index });
+  };
+
+  if (items.length === 0) {
+    return (
+      <p className="cart-empty">Cart is empty</p>
+    );
+  }
+  return (
+    <>
+        <p className="total">
+        Total price:{" "}
+        ${totalPrice}
+      </p>
+      {items.map((item, index) => (
+        <CartItem
+          handleRemove={handleRemove}
+          key={index}
+          product={item}
+          index={index}
+        />
+      ))}
+    </>
+    
+  );
+}
+
+
+
