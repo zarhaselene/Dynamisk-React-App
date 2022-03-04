572a5fa1143fbd4fce50d6ac5a6615f6211b758f
Melissa Zarinnegar <melissahh@live.se>
2022-03-04 16:08:23 +0100

    Product update

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,58 +1,72 @@
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import '../styles/Product.css'
-
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch } from "../components/Cart";
+import "../styles/Product.css";
 
 function Product({ Products }) {
+	const dispatch = useDispatch();
 
-  const addToCart = (item) => {
-    console.log(item);
-  }
+	const addToCart = (item) => {
+		console.log(item);
+		dispatch({ type: "ADD", item });
+	};
+	const [product, setProduct] = useState({});
+	const params = useParams();
 
-  const [product, setProduct] = useState({});
-  const params = useParams();
+	const fetchData = async () => {
+		try {
+			const response = await fetch(
+				"https://codexplained.se/shoes.php?id=" + params.id
+			);
+			const data = await response.json();
+			console.log(data);
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch('https://codexplained.se/shoes.php?id=' + params.id);
-      const data = await response.json();
-      console.log(data);
+			setProduct(data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-      setProduct(data);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+	useEffect(() => {
+		fetchData();
+	});
 
-  useEffect(() => {
-    fetchData();
-  });
+	return (
+		<div className="productpage-container" key={product.id}>
+			<div className="productpage-wrapper">
+				{/* Image */}
+				<div className="image-container">
+					<img className="product-image" src={product.url} alt="" />
+				</div>
+				{/* Info */}
+				<div className="info-container">
+					<h1 className="product-title">{product.title}</h1>
+					<p className="product-desc">{product.description}</p>
+					<span className="product-price">${product.price}</span>
+					<p className="product-storage">In stock: {product.storage}</p>
 
-
-  return (
-    <div className='productpage-container' key={product.id}>
-      <div className='productpage'>
-        <div className='image-product'>
-          <img src={product.url} alt='' height='400' width='400' />
-        </div>
-        <div className='title-product'>
-          <h1>{product.title}</h1>
-        </div>
-        <div className='price-product'>
-          <h3>{product.price} kr</h3>
-        </div>
-        <div className='price-storage'>
-          <h3>Lagerstatus: {product.storage} st</h3>
-        </div>
-        <div className='info-product'>
-          <p>{product.description}</p>
-        </div>
-        <div className='btn-position'>
-          <button onClick={() => addToCart(product)}>Lägg i kundvagnen</button>
-        </div>
-      </div>
-    </div>
-  )
+					<div className="product-filter-container">
+						<div className="product-filter">
+							<select className="product-filter-size">
+								<option disabled selected>
+									{" "}
+									Size
+								</option>
+								<option>38</option>
+								<option>39</option>
+								<option>40</option>
+								<option>41</option>
+							</select>
+						</div>
+					</div>
+					{/* Add to cart */}
+					<div className="product-add-btn">
+						<button onClick={() => addToCart(product)}>Add to cart</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Product
+export default Product;
