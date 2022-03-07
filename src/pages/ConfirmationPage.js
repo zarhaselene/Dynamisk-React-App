730c2d311838d6e449534ce02f63ab5c5145f12d
Melissa Zarinnegar <melissahh@live.se>
2022-03-08 00:13:10 +0100

    fixed error VM68:2 Uncaught ReferenceError: process is not defined

--- src/pages/ConfirmationPage.js
+++ src/pages/ConfirmationPage.js
@@ -5,7 +5,7 @@ import '../styles/ConfirmationPage.css'
 function ConfirmationPage() {
   return (
       <div className='imagestyle'>
-    <img src={ Thankyou } alt='' height="10%" width="60%" />
+    <img src={ Thankyou } alt='' height="10%" width="50%" />
     </div>
   )
 }
